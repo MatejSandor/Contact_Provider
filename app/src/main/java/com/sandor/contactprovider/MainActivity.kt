21f8fcbdb60d57ffdb3cae91fbe7e3e86d6dc7f8
Matej Šándor <matejko.sandor@gmail.com>
2020-05-09 17:13:51 +0200

    added content resolver to get the data from the database

--- app/src/main/java/com/sandor/contactprovider/MainActivity.kt
+++ app/src/main/java/com/sandor/contactprovider/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.sandor.contactprovider
 
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +20,15 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            Log.d(TAG,"fab onClick: called")
+            val projection = arrayOf(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+            val cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                projection,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+            )
+
         }
     }
 
